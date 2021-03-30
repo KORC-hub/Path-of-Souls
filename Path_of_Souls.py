3bbf5e7152fd82fff8b3f0e3f3b57cb97c6c1913
Rogers <rogerscardonakevin@gmail.com>
2021-03-30 17:05:00 -0500

    agregado los nuevos sprite y la animacion del jugador

--- Path_of_Souls.py
+++ Path_of_Souls.py
@@ -1,65 +1,32 @@
 import pygame as pg
+import sys
+from data import player
 
 height = 563 # alto
 width = 1001 # ancho
-white = (255, 255, 255)
-black = (0, 0, 0)
+white = (255, 255, 255) # color blanco
+black = (0, 0, 0) # color negro
 
-pg.init()
+pg.init() 
 screen = pg.display.set_mode((width,height))
-pg.display.set_caption("Shooter")
+pg.display.set_caption("Path of souls")
 clock = pg.time.Clock()
+teclas = [False,False,False,False]
+player= player.personaje((width/2, height/2),teclas)
+posicion_base = [0,0]
+fps = 10
 
-#La siguiente clase abraca todo lo que tiene que ver con el jugador 
-class Player(pg.sprite.Sprite):
-  def __init__(self):
-    super().__init__()
-    self.image = pg.image.load("C:/Lenguajes/Python/Pygame/Path of Souls/resources/graphics/Player/avatar_1.png").convert()
-    self.image.set_colorkey(white)
-    self.rect = self.image.get_rect()
-    self.rect.centerx = width // 2
-    self.rect.bottom = height // 2 
-    self.speed = 3
-
-  def update(self):
-    self.speed_x = 0
-    self.speed_y = 0
-    keystate = pg.key.get_pressed()
-    if keystate[pg.K_a]:
-      self.speed_x -= self.speed
-    if keystate[pg.K_d]:
-      self.speed_x += self.speed
-    self.rect.x += self.speed_x
-
-    if keystate[pg.K_w]:
-      self.speed_y -= self.speed
-    if keystate[pg.K_s]:
-      self.speed_y += self.speed
-    self.rect.y += self.speed_y
-
-    if self.rect.x > 880:
-      self.rect.x = 880
-    if self.rect.x < 47:
-      self.rect.x = 47
-    if self.rect.y > 437:
-      self.rect.y = 437
-    if self.rect.y < 47:
-      self.rect.y = 47
-
-  def shoot(self):
-    bullet = Bullet(self.rect.centerx, self.rect.top)
-    all_sprites.add(bullet)
-    bullets.add(bullet)
 
 all_sprites = pg.sprite.Group()
 
-player = Player()
-all_sprites.add(player)
-
 class Bullet(pg.sprite.Sprite):
-  def __init__(self, x, y):
+  def __init__(self, mx, my):
     super().__init__()
+    self.image = pg.Surface((50,10))
+    self.image.fill((255,0,0))
 
+  def update(self):
+    self.rect.y -= 5
 
 background = pg.image.load("C:/Lenguajes/Python/Pygame/Path of Souls/resources/graphics/menu.png")
 sala1 = pg.image.load("C:/Lenguajes/Python/Pygame/Path of Souls/resources/graphics/Sala_1.png")
@@ -72,13 +39,12 @@ while running:
   play = False
 
   while menu:
-
     screen.blit(background,[0,0])
     for event in pg.event.get():
     # check for closing window
       if event.type == pg.QUIT:
-        menu = False
-        play = False
+        pg.quit()
+        sys.exit()
       if event.type == pg.KEYDOWN:
         if event.key == pg.K_RETURN:
           menu = False
@@ -87,25 +53,22 @@ while running:
     
   while play:
 
-    clock.tick(60)
+    clock.tick(fps)
     # Process input (events)
     for event in pg.event.get():
       # check for closing window
       if event.type == pg.QUIT:
-        running = False
+        pg.quit()
+        sys.exit()
       elif event.type == pg.MOUSEBUTTONUP:
           player.shoot() 
       
-    pos = pg.mouse.get_pos()
-    print(pos,"|",player.rect.x,player.rect.y)
+    #print(player.pos_m,"|",player.rect.x,player.rect.y)
 
+    player.handle_event(event)
+    screen.blit(sala1, posicion_base)
+    screen.blit(player.image, player.rect)
 
-    # Update
-    all_sprites.update()
-
-    #Draw / Render
-    screen.blit(sala1,[0,0])
-    all_sprites.draw(screen)
     # *after* drawing everything, flip the display.
     pg.display.flip()
 
@@ -116,4 +79,3 @@ while running:
       #while sala5:
       #while final:
       
-pg.quit()
---
+++ data/player.py
@@ -0,0 +1,80 @@
+import pygame as pg
+
+class personaje(pg.sprite.Sprite):
+    def __init__(self,position,teclas):
+        self.sheet = pg.image.load('C:/Lenguajes/Python/Pygame/Path of Souls/resources/graphics/Player/Ymir.png')
+        self.sheet.set_clip(pg.Rect(0,0,60,100))
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+        self.rect = self.image.get_rect()
+        self.rect.topleft = position
+        self.frame = 0
+        self.speed = 8
+        self.left_states  = { 0: (0,100,60,100), 1: (60,100,60,100), 2: (120,100,60,100), 3: (180,100,60,100)}
+        self.right_states = { 0: (0,200,60,100), 1: (60,200,60,100), 2: (120,200,60,100), 3: (180,200,60,100)}
+        self.up_states    = { 0: (0,300,60,100), 1: (60,300,60,100), 2: (120,300,60,100), 3: (180,300,60,100)}
+        self.down_states  = { 0: (0,0,60,100), 1: (60,0,60,100), 2: (120,0,60,100), 3: (180,0,60,100)}
+
+    def get_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        return frame_set[self.frame]
+
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.sheet.set_clip(pg.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.sheet.set_clip(pg.Rect(clipped_rect))
+        return clipped_rect
+
+    def update(self, direction):
+        if direction == 'left':
+            self.clip(self.left_states)
+            self.rect.x -= self.speed
+        if direction == 'right':
+            self.clip(self.right_states)
+            self.rect.x += self.speed
+        if direction == 'up':
+            self.clip(self.up_states)
+            self.rect.y -= self.speed
+        if direction == 'down':
+            self.clip(self.down_states)
+            self.rect.y += self.speed
+
+        if direction == 'stand_left':
+            self.clip(self.left_states[0])
+        if direction == 'stand_right':
+            self.clip(self.right_states[0])
+        if direction == 'stand_up':
+            self.clip(self.up_states[0])
+        if direction == 'stand_down':
+            self.clip(self.down_states[0])
+
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+
+    def handle_event(self, event):
+        if event.type == pg.QUIT:
+            game_over = True
+
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                self.update('left')
+            if event.key == pg.K_RIGHT:
+                self.update('right')
+            if event.key == pg.K_UP:
+                self.update('up')
+            if event.key == pg.K_DOWN:
+                self.update('down')
+
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_LEFT:
+                self.update('stand_left')
+            if event.key == pg.K_RIGHT:
+                self.update('stand_right')
+            if event.key == pg.K_UP:
+                self.update('stand_up')
+            if event.key == pg.K_DOWN:
+                self.update('stand_down')
+
+    def shoot(self):
+      return Bullet(pg.mouse.get_pos()[0],pg.mouse.get_pos()[1])
