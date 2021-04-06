eafe1b3f78d1c34302026a6ccfc1b44253798a6a
Rogers <rogerscardonakevin@gmail.com>
2021-04-05 19:36:37 -0500

    Se agrego el Guia a el programa

--- Path_of_Souls.py
+++ Path_of_Souls.py
@@ -16,62 +16,62 @@ sala1 = False
 x = 0
 while running:
 
-  while menu:
+ while menu:
 
-    screen.blit(c.background,c.posicion_base)
-    for event in pg.event.get():
-      if event.type == pg.QUIT:
-        pg.quit()
-        sys.exit()
+  screen.blit(c.background,c.posicion_base)
+  for event in pg.event.get():
+   if event.type == pg.QUIT:
+    pg.quit()
+    sys.exit()
 
-      if event.type == pg.KEYDOWN:
-        if event.key == pg.K_RETURN:
-          menu = False
-          sala1 = True
+   if event.type == pg.KEYDOWN:
+    if event.key == pg.K_RETURN:
+     menu = False
+     sala1 = True
 
-    pg.display.flip()
-    
-  while sala1:
+  pg.display.flip()
+   
+ while sala1:
 
-    clock.tick(c.fps)
+  clock.tick(c.fps)
 
-    for event in pg.event.get():
-      if event.type == pg.QUIT:
-        pg.quit()
-        sys.exit()
+  for event in pg.event.get():
+   if event.type == pg.QUIT:
+    pg.quit()
+    sys.exit()
 
-    pos = pg.mouse.get_pos()
-    print(pos)
+  pos = pg.mouse.get_pos()
+  print(pos,)
 
-    player.handle_event(event)
-    screen.blit(c.sala1, c.posicion_base)
-    screen.blit(player.image, player.rect)
+  player.handle_event(event)
+  screen.blit(c.sala1, c.posicion_base)
+  screen.blit(player.image, player.rect)
 
-    if x == 0:
-      sala1 = False
-      introduccion = True
+  if x == 0:
+   sala1 = False
+   introduccion = True
 
-    pg.display.flip()
+  pg.display.flip()
 
-  while introduccion:
+ while introduccion:
 
-    if x == 0:
-      pg.time.wait(500)
-      x += 1
+  if x == 0:
+   pg.time.wait(500)
+   x += 1
 
-    screen.blit(c.Guide, c.posicion_texto)
-    for event in pg.event.get():
-      if event.type == pg.QUIT:
-        pg.quit()
-        sys.exit()
+  screen.blit(c.Guide, c.posicion_texto)
+  for event in pg.event.get():
+   if event.type == pg.QUIT:
+    pg.quit()
+    sys.exit()
 
-    if event.type == pg.KEYDOWN:
-      if event.key == pg.K_RETURN:
-        introduccion = False
-        sala1 = True
+  if event.type == pg.KEYDOWN:
+   if event.key == pg.K_LEFT or event.key == pg.K_RIGHT or event.key == pg.K_UP or event.key == pg.K_DOWN: 
+    introduccion = False
+    sala1 = True
 
-    pg.display.flip()
+  pg.display.flip()
 
-  #while sala2:
-  #while sala3:
-  #while final:
+ #while sala2:
+ #while sala3:
+ #while final:
